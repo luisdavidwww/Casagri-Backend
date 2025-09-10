7ffdfe120c76f8031bb711ec50cf75a263f598f8
LuisDavid <luisdavid25torrealba@gmail.com>
2025-09-10 17:00:23 -0400

    actualizacion de oficina

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,6 +1,15 @@
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { usuariosGet, usuariosPost } = require('../controllers/usuarios');
+
+
+
+const {
+    validarCampos,
+} = require('../middlewares');
+
 
 
 const router = Router();
@@ -9,6 +18,10 @@ const router = Router();
 router.get('/', usuariosGet );
 
 router.post('/',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña debe de ser más de 6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    validarCampos
 ], usuariosPost );
 
 
